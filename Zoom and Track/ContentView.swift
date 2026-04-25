238d0081553eff1257ac8ac14a538d1b1de464d9
paulrandall <juggleypaul@gmail.com>
2026-04-25 22:32:50 +0100

    Initial Commit

---
+++ Zoom and Track/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zoom and Track
+//
+//  Created by Paul Randall on 25/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zoom and Track/Zoom_and_TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zoom_and_TrackApp.swift
+//  Zoom and Track
+//
+//  Created by Paul Randall on 25/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Zoom_and_TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
